1c8144f0bb347985a6c0cce15e132a3042a6c440
w1erley <danor.play3@gmail.com>
2024-06-14 10:48:10 +0300

    Changed ImageController

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Http\Requests\UploadImageRequest;
-
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
@@ -23,13 +20,20 @@ class ImageController extends Controller
 
         $imageURL = null;
 
-        if($request->hasFile("image")) {
+        if ($request->hasFile("image")) {
             $image = $request->file("image");
-            $filename = time() . '.' . $image->getClientOriginalExtension();  //uuid
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+
+            // Check if 'profile' folder exists, create it if not
+            if (!Storage::disk('public')->exists('profile')) {
+                Storage::disk('public')->makeDirectory('profile');
+            }
+
             Image::make($image)->resize(300, 300)->save(public_path('storage/profile/' . $filename), 60);
 
             $imageURL = 'profile/' . $filename;
 
+            // Delete old image if exists
             Storage::disk('public')->delete($user->image ?? '');
         }
 
